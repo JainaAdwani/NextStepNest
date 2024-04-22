3ed7a5c749c3f601dd7b8fd31259a3550ad7561d
Jaina Adwani <jaina@192.168.1.3>
2024-04-23 02:49:16 +0530

    Profile and Update API

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header className='bg-black shadow-md hover:bg-zinc-900'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
-                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap cursor-pointer'>
                     <span className='text-red-700 text-3xl'>Next</span>
                     <span className='text-red-600 text-3xl'>Step</span>
                     <span className='text-red-500 text-3xl'>Nest</span>
@@ -16,17 +16,17 @@ export default function Header() {
             </Link>
             <ul className='flex gap-4 items-center'>
                 <Link to='/'>
-                <li className='hidden sm:inline text-white hover:underline hover:text-red-700 hover:text-xl'>Home</li>
+                    <li className='hidden sm:inline text-white hover:underline hover:text-red-700 hover:text-xl'>Home</li>
                 </Link>
                 <Link to='/about'>
-                <li className='hidden sm:inline text-white hover:underline hover:text-red-700 hover:text-xl'>About</li>
+                    <li className='hidden sm:inline text-white hover:underline hover:text-red-700 hover:text-xl'>About</li>
                 </Link>
-                <Link to='/sign-in'>
+                <Link to='/profile'>
                     {currentUser ? 
                         (<img className='rounded-full h-8 w-8 object-cover' src={currentUser.avatar} alt='profile'/>)
-                        : (<li className=' text-slate-700 hover:underline'> Sign in</li>)
+                        : (<li className='hidden sm:inline text-white hover:underline hover:text-red-700 hover:text-xl'>SignIn</li>)
                     }
-                <li className='hidden sm:inline text-white hover:underline hover:text-red-700 hover:text-xl'>SignIn</li>
+                
                 </Link>
             </ul>
         </div>
